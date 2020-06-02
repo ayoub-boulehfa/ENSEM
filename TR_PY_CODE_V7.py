895d5a66fc38c3d19763910cb20a58a7bbdf5c5f
ayoub-boulehfa <66218940+ayoub-boulehfa@users.noreply.github.com>
2020-06-02 22:16:44 +0100

    Update TR_PY_CODE_V7.py

--- TR_PY_CODE_V7.py
+++ TR_PY_CODE_V7.py
@@ -1,58 +1,163 @@
-#this code was created bye ENSEM students
+import csv
 from serial import *
-import os
 from time import sleep
 from datetime import datetime
+import hashlib
+import numpy as np
 import matplotlib.pyplot as plt
+import keyboard
 
+class Graphes:
 
-print("""plateforme temps réels permettant la mesure du temps du contrôle et la manipulation des mesures prises par des capteurs de température, pression et volume à l’aide d’une carte Arduino.
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
+    colors = ['b', 'r', 'g']
+
+    def __init__(self, step, x_width):
+        self.fig = plt.figure(figsize=(10, 10))
+        self.init_fig()
+        self.show_credits()
+        self.axs = [self.fig.add_subplot(311), self.fig.add_subplot(312), self.fig.add_subplot(313)]
+        self.fig.tight_layout(pad=5.0)
+        self.init_axs()
+        self.timestamp = [0, ]
+        self.step = step
+        self.x_width = x_width
+        self.y_axs = [[], [], []]
+        self.statistics = [None, None, None]
+        self.last_values = [None, None, None]
+
+    def init_fig(self):
+        self.fig.canvas.toolbar.pack_forget()
+        self.fig.set_facecolor('lavender')
+
+    def init_axs(self):
+        self.axs[0].set_ylabel("Temperature (C)", fontsize=14)
+        self.axs[1].set_ylabel("Pression (bar)", fontsize=14)
+        self.axs[2].set_ylabel("Debit (m^3/s)", fontsize=14)
+
+        for ax in self.axs:
+            ax.set_xlabel("temps (s)")
+            ax.xaxis.set_label_coords(1.04, -0.025)
+            ax.grid(True)
+            ax.set_facecolor((0.9, 0.9, 0.9))
+
+    def show_credits(self):
+        credits_title ="""
+plateforme temps réels permettant la mesure du temps du contrôle
+et la manipulationdes mesures prises par des capteurs de
+température,pression et volume à l’aide d’une carte Arduino."""
+        credits_names="""
 Le projet est réalisé par :
--	Lamiae Touti
--	Oumniya Ramdi 
--	Ayoub Boulehfa
--	Oussama Moudnib
-\t                 Encadré par H.Medromi""")
-sleep(5)
-os.system('cls' if os.name == 'nt' else 'clear')
-com="com"+str(input("Entrez le numéro de port COM connecté à l'arduino:"))
-i=-1
-plt.axis([0, 10, 0, 100])
-plt.axis([0, 2, 0, 50])
-plt.scatter(i,0,color='r',label="Temp")
-plt.scatter(i,0,color='b',label="Pres")
-plt.scatter(i,0,color='g',label="Debit")
-plt.legend()
-i=0
-try:
-    with Serial(port=com, baudrate=9600, timeout=1, writeTimeout=1) as port_serie:
-        while True:
-            ligne = port_serie.readline()
-            ligne = str(ligne)
-            ligne=ligne[2:-5]
-            L=ligne.split()
-            if L != [] :
-                now = datetime.now()
-                current_time = now.strftime("%H:%M:%S")
-                Temp=L[0]
-                Pres=L[1]
-                debit=L[2]
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print("Temperature \t: \t"+str(Temp)+"\nPression \t: \t"+str(Pres)+"\nDébit \t\t: \t"+str(debit))
-                file_object = open('base de données.txt', 'a')
-                file_object.write(str(current_time)+"\t\tTemperature \t:"+str(Temp)+"\tPression \t:"+str(Pres)+"\tDébit \t"+str(debit)+"\n")
-                file_object.close()
-                plt.axis([0, i+2, 0, 50])
-                plt.scatter(i,Temp,color='r',label="Temp")
-                plt.scatter(i,Pres,color='b',label="Pres")
-                plt.scatter(i,debit,color='g',label="Debit")
-                i+=1
-                plt.pause(0.01)
-                               
-                
-                
-except:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print ("aucune carte n'est connecté au port "+com)
+Lamiae Touti
+Oumniya Ramdi 
+Ayoub Boulehfa
+Oussama Moudnib
+"""
+        credits_MTR='Encadré par H.Medromi'
+        plt.text(0.5, 1, credits_title, ha='center', va='center', fontsize=15)
+        plt.text(0.0, 0, credits_names, ha='center', va='center', fontsize=10)
+        plt.text(1, 0, credits_MTR, ha='center', va='center', fontsize=11)
+        plt.axis('off')
+        mng = plt.get_current_fig_manager()
+        mng.full_screen_toggle()
+        plt.pause(3)
+        plt.clf()
 
-time.sleep(5)
+    def update(self, donnees):
+        for i, (value, ax, y_values, color) in enumerate(zip(donnees, self.axs, self.y_axs, self.colors)):
+            y_values.append(value)
+            ax.plot(self.timestamp, y_values, color=color, antialiased=True)
+            x_min, x_max = max(0, self.timestamp[-1] - self.x_width), max(self.timestamp[-1], self.x_width)
+            ax.set_xlim(left=x_min, right=x_max)
+            ax.xaxis.set_ticks(np.arange(x_min, x_max, self.step))
+
+            if self.last_values[i]:
+                self.last_values[i].set_visible(False)
+            self.last_values[i] = ax.text(self.timestamp[-1] + self.step/10, value, value)
+    
+            if self.statistics[i]:
+                self.statistics[i].set_visible(False)
+            statistics_string = 'MIN: ' + "{:.2f}".format(min(y_values)) + '  ||  MAX: ' + "{:.2f}".format(max(y_values)) + '  ||  AVG: ' + "{:.2f}".format(np.mean(y_values))
+            self.statistics[i] = ax.annotate(statistics_string, (0.25, -0.18), xycoords='axes fraction', va='top', fontsize=12, bbox=self.props)
+
+        plt.pause(self.step)
+        self.timestamp.append(self.timestamp[-1] + self.step)
+
+class BaseDonnees:
+
+    def __init__(self, name):
+        try:
+            self.file = open(name + '.csv', mode='w')
+            self.writer = csv.writer(self.file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            self.writer.writerow(["Date", "Temperature", "Pression", "Debit"])
+        except:
+            print("Error opening database")
+
+    def write(self, temperature, pression, debit):
+        self.writer.writerow([datetime.now().strftime("%H:%M:%S"), str(temperature), str(pression), str(debit)])
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
+
+class SerialPort:
+
+    def __init__(self):
+        com = "com" + SerialPort.get_numero_port()
+        self.file = Serial(port=com, baudrate=9600, timeout=1, writeTimeout=1)
+
+    def get_numero_port():
+        port = input("Entrez le numéro de port COM connecté à l'arduino: ")
+        correct = False
+        while not correct:
+            try:
+                correct = int(port) >= 0
+                if not correct:
+                    raise ValueError
+            except ValueError:
+                port = input("Erreur: veuillez enter un entier positif: ")
+        return port
+
+    def get_data(self):
+        return self.file.readline()[0: -5].split()
+
+    def close(self):
+        self.file.close()
+
+class Identification:
+
+    username = "21232f297a57a5a743894a0e4a801fc3"
+    password = "b7fb5f77d7c3efd9358ea1c55bb74734"
+
+    def __init__(self):
+        while True:
+            username = input("Identifiant: ")
+            password = input("Mot de passe: ")
+            if hashlib.md5(username.encode()).hexdigest() != Identification.username or hashlib.md5(password.encode()).hexdigest() != Identification.password:
+                print("Les informations d'identification sont invalides, veuillez reessayer.\n")
+            else:
+                break
+
+if __name__ == "__main__":
+    serial_port = None
+    base_donnees = None
+    Identification()
+    try:
+        serial_port = SerialPort()
+        graphes = Graphes(2, 60)
+        
+        base_donnees = BaseDonnees("database")
+
+        while True:
+            donnees = serial_port.get_data()
+            if donnees:
+                donnees = list(map(lambda s: float(s), donnees))
+                graphes.update(donnees)
+                base_donnees.write(*donnees)
+                
+    except:
+        if serial_port:
+            serial_port.close()
+        if base_donnees:
+            base_donnees.close()
+        print("Une erreur est survenue")
